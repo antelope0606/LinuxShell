37fb0256465b2699dd958feaa1ceebbb2310f8ad
antelope0606 <anantelope@hotmail.com>
2021-07-13 17:58:30 +0900

    h001 comment added

--- h001_ConfirmEnv.sh
+++ h001_ConfirmEnv.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # confirm env
 echo check version of Linux
+echo ========================
+echo 'cat /proc/version'
 cat /proc/version
+
+echo ========================
+echo 'cat /etc/os-release'
 cat /etc/os-release
+
+echo ========================
+echo 'uname -a'
 uname -a
 
