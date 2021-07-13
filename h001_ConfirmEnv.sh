37f68a161ffaf189a2806b2f46d307d697ffd156
antelope0606 <anantelope@hotmail.com>
2021-07-13 18:43:23 +0900

    h001 add cpu

--- h001_ConfirmEnv.sh
+++ h001_ConfirmEnv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # confirm env
+
+echo ========================
 echo check version of Linux
 echo ========================
 echo 'cat /proc/version'
@@ -12,4 +14,14 @@ cat /etc/os-release
 echo ========================
 echo 'uname -a'
 uname -a
+
+echo ========================
+echo check CPU
+echo ========================
+echo cat /proc/cpuinfo
+cat /proc/cpuinfo
+
+echo ========================
+echo lscpu
+lscpu
 
