20aedd383be55772096728de6ce06b5ef56024d8
antelope0606 <anantelope@hotmail.com>
2021-07-15 08:21:09 +0900

    a002 function added

--- a002_sys_info_page.sh
+++ a002_sys_info_page.sh
@@ -1,45 +1,29 @@
 #!/bin/bash
 # Program to output a system information page
-
 TITLE="System Information Report For $HOSTNAME"
 CURRENT_TIME="$(date +"%x %r %Z")"
 TIMESTAMP="Generated $CURRENT_TIME, by $USER"
-echo "<html>
-<head>
-<title>$TITLE</title>
-</head>
-<body>
-Variables and Constants
-<h1>$TITLE</h1>
-<p>$TIMESTAMP</p>
-</body>
-</html>"
-
-TITLE="System Information Report For $HOSTNAME"
-CURRENT_TIME="$(date +"%x %r %Z")"
-TIMESTAMP="Generated $CURRENT_TIME, by $USER"
+report_uptime () {
+return
+}
+report_disk_space () {
+return
+}
+report_home_space () {
+return
+}
 cat << _EOF_
 <html>
 <head>
 <title>$TITLE</title>
+
 </head>
 <body>
 <h1>$TITLE</h1>
 <p>$TIMESTAMP</p>
+$(report_uptime)
+$(report_disk_space)
+$(report_home_space)
 </body>
 </html>
 _EOF_
-
-FTP_SERVER=ftp.nl.debian.org
-FTP_PATH=/debian/dists/stretch/main/installer-amd64/current/images/
-cdrom REMOTE_FILE=debian-cd_info.tar.gz
-ftp -n <<- _EOF_
-open $FTP_SERVER
-user anonymous me@linuxbox
-cd $FTP_PATH
-hash
-get $REMOTE_FILE
-bye
-_EOF_
-ls -l "$REMOTE_FILE"
-
