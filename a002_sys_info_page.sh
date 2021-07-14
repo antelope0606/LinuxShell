736b00a041833ba857c70c6f64e986a10a88a27f
antelope0606 <anantelope@hotmail.com>
2021-07-15 08:25:32 +0900

    a002 function completed

--- a002_sys_info_page.sh
+++ a002_sys_info_page.sh
@@ -4,12 +4,24 @@ TITLE="System Information Report For $HOSTNAME"
 CURRENT_TIME="$(date +"%x %r %Z")"
 TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 report_uptime () {
+cat <<- _EOF_
+<h2>System Uptime</h2>
+<pre>$(uptime)</pre>
+_EOF_
 return
 }
 report_disk_space () {
+cat <<- _EOF_
+<h2>Disk Space Utilization</h2>
+<pre>$(df -h)</pre>
+_EOF_
 return
 }
 report_home_space () {
+cat <<- _EOF_
+<h2>Home Space Utilization</h2>
+<pre>$(du -sh /home/*)</pre>
+_EOF_
 return
 }
 cat << _EOF_
