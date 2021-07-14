1fb4f1b60264396e8dc9948babadcd052804e037
antelope0606 <anantelope@hotmail.com>
2021-07-14 16:18:08 +0900

    a002 << added

--- a002_sys_info_page.sh
+++ a002_sys_info_page.sh
@@ -14,3 +14,32 @@ Variables and Constants
 <p>$TIMESTAMP</p>
 </body>
 </html>"
+
+TITLE="System Information Report For $HOSTNAME"
+CURRENT_TIME="$(date +"%x %r %Z")"
+TIMESTAMP="Generated $CURRENT_TIME, by $USER"
+cat << _EOF_
+<html>
+<head>
+<title>$TITLE</title>
+</head>
+<body>
+<h1>$TITLE</h1>
+<p>$TIMESTAMP</p>
+</body>
+</html>
+_EOF_
+
+FTP_SERVER=ftp.nl.debian.org
+FTP_PATH=/debian/dists/stretch/main/installer-amd64/current/images/
+cdrom REMOTE_FILE=debian-cd_info.tar.gz
+ftp -n <<- _EOF_
+open $FTP_SERVER
+user anonymous me@linuxbox
+cd $FTP_PATH
+hash
+get $REMOTE_FILE
+bye
+_EOF_
+ls -l "$REMOTE_FILE"
+
