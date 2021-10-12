60a71c3237136e00fe3b006cf8439b3ee3d6973c
antelope0606 <anantelope@hotmail.com>
2021-10-12 21:29:22 +0900

    add a03.sh

---
+++ a03.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is our first script.
+echo 'This is the bash for 3rd chapter'
+echo '# ls -altF ~'
+echo "$(ls -altF ~)"
+echo '# file ~'
+echo "$(file ~)"
+echo '# less /etc/passwd'
+echo "$(less /etc/passwd)"
