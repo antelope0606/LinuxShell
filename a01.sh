3cac91581d9ae7cbaad3d91343469189c090ff75
antelope0606 <anantelope@hotmail.com>
2021-10-12 18:05:02 +0900

    add a01.sh

---
+++ a01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This is our first script.
+echo 'This is the bash for 1st chapter'
+echo 'bash is an acronym for Bourne Again Shell'
+echo '# date'
+echo "$(date)"
+echo '# cal'
+echo "$(cal)"
+echo '# df -h'
+echo "$(df -h)"
+echo '# free -h'
+echo "$(free -h)"
