4ff11e8e77f0170f3b7511ea4b5659d7bbaf2fb7
antelope0606 <anantelope@hotmail.com>
2021-10-29 20:27:06 +0900

    add a04.sh

---
+++ a04.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is our first script.
+echo 'This is the bash for 4th chapter'
+echo 'bash is an acronym for Bourne Again Shell'
+echo '# make dir1'
+echo "$(mkdir dir1)"
+echo '# create file1'
+echo "$(touch file1)"
+
